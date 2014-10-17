9a6f9a07d5015c22776df5338f8fdefb8ed58dab
kongdeju <kongdeju@kong-OptiPlex-330.(none)>
2014-10-17 17:14:34 +0800

    final edition

--- scripts/ouput.sh
+++ scripts/ouput.sh
@@ -6,7 +6,7 @@ e=$3
 f=`expr $b - $a`
 c=`expr $a + $f / 2 + $e`
 g=`expr $f / 2`
-d=`expr $c + $e`
+d=`expr $c - 1`
 for a in `ls *.rank2`
 do
 awk '{print $1,"\t",$5,"\t",$9,"\t",$11,"\t",substr($4,18,4)+substr($4,29,4),"\t",substr($8,18,4)+substr($8,29,4)+'"$c"',"\t",substr($12,18,4)+substr($12,29,4)+'"$g"',"\t",substr($8,18,4)-substr($4,18,4)+'"$d"'}' $a >$a.output
